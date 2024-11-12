22e5c6423010f3411cf3d411f4637cfe37ae08c6
rakesh0523 <238r1a0523@cmrec.ac.in>
2024-11-12 10:25:58 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class main
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      if(n%2==0)
+      {
+        System.out.println("mandi");
+      }
+      else
+      {
+        System.out.println("biryani");
+      }
+    }
+  }
